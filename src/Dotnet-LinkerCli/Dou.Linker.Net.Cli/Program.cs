1243aafb70e9ab2d3c3083d2b3402b1eeabadc2e
Daniel Semedo <danpin@microsoft.com>
2017-12-26 12:08:44 -0200

    StreamReader lendo por linha e salvando na variavel line e samples de arquivos XML para testes.

--- src/Dotnet-LinkerCli/Dou.Linker.Net.Cli/Program.cs
+++ src/Dotnet-LinkerCli/Dou.Linker.Net.Cli/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.IO;
 
 namespace Dou.Linker.Net.Cli
 {
+  
     class Program
     {
+        private static string file = @"C:\Projects\DOU-Linker\src\XmlSamples\Sample1.xml";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello DOU");
+            var reader = new XmlReader();
+
+            reader.ReadXmlFile(file);
+
+            Console.ReadLine();
         }
     }
 }
+
---
+++ src/Dotnet-LinkerCli/Dou.Linker.Net.Cli/XmlReader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Dou.Linker.Net.Cli
+{
+    public class XmlReader
+    {
+        public void ReadXmlFile(string xmlFile)
+        {
+            try
+            {
+                // Fará a leitura do XML dos artigos e deverá retornar a lei correspondente do XML através da lógica de Parse.
+
+                using (StreamReader sr = new StreamReader(xmlFile))
+                {
+                    string line;
+                    // Read and display lines from the file until the end of 
+                    // the file is reached.
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("O arquivo nao pode ser lido");
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+
+
+
+
+    }
+
+}
