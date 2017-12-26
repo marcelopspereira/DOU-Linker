dbc3f10188becb04e3d2bbdec9c7e6d591d06e2e
Daniel Semedo <danpin@microsoft.com>
2017-12-26 10:22:00 -0200

    added files

---
+++ src/Dotnet-LinkerCli/Dou.Linker.Net.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dou.Linker.Net.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello DOU");
+        }
+    }
+}
