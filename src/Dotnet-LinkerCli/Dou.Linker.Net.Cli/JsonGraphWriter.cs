b73fcf36264b4639df8afae33f21edd8ae8bace6
Daniel Semedo <danpin@microsoft.com>
2017-12-26 15:33:19 -0200

    Json Writer

---
+++ src/Dotnet-LinkerCli/Dou.Linker.Net.Cli/JsonGraphWriter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Dou.Linker.Net.Cli
+{
+    class JsonGraphWriter
+    {
+        public void XMLtoJsonWriter()
+        {
+            // Get the directories currently on the C drive.
+            DirectoryInfo[] cDirs = new DirectoryInfo(@"c:\").GetDirectories();
+
+            // Write each directory name to a file.
+            using (StreamWriter sw = new StreamWriter("CDriveDirs.txt"))
+            {
+                foreach (DirectoryInfo dir in cDirs)
+                {
+                    sw.WriteLine(dir.Name);
+
+                }
+            }
+
+            // Read and show each line from the file.
+            string line = "";
+            using (StreamReader sr = new StreamReader("CDriveDirs.txt"))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+
+
+
+
+
+        }
+
+
+
+    }
+}
--- src/Dotnet-LinkerCli/Dou.Linker.Net.Cli/Program.cs
+++ src/Dotnet-LinkerCli/Dou.Linker.Net.Cli/Program.cs
@@ -6,7 +6,10 @@ namespace Dou.Linker.Net.Cli
   
     class Program
     {
-        private static string file = @"C:\Projects\DOU-Linker\src\XmlSamples\Sample1.xml";
+        private static int i = 1;
+
+       
+        private static string file = @"C:\Projects\DOU-Linker\src\XmlSamples\Sample"+ i + ".xml";
 
         static void Main(string[] args)
         {
@@ -14,11 +17,22 @@ namespace Dou.Linker.Net.Cli
 
 
             //Leitura do XML e armazenamento da linha na variavel line
-            reader.ReadXmlFile(file);
+            for (i = 1; i < 14; i++)
+            {
+                file = @"C:\Projects\DOU-Linker\src\XmlSamples\Sample" + i + ".xml";
+                reader.ReadXmlFile(file);
+                
+            }
 
             //Analise da variavel line para captura dos termos (lei, portaria, projeto de lei e etc...)
 
+
+
+
+
             //geracao do arquivo de log no formato JSON e grafo (1 Artigo XML = 1 Documento JSON do Grafo)
+
+
 
 
             Console.ReadLine();
