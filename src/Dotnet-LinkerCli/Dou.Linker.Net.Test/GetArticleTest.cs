b247c5035407a7edf1c0d9601ab241432473d712
Daniel Semedo <danpin@microsoft.com>
2018-01-12 14:23:22 -0200

    unit test covering the xml input and the main method xmlReader()

--- src/Dotnet-LinkerCli/Dou.Linker.Net.Test/GetArticleTest.cs
+++ src/Dotnet-LinkerCli/Dou.Linker.Net.Test/GetArticleTest.cs
@@ -1,12 +1,25 @@
 using System;
 using Xunit;
+using Dou.Linker.Net.Cli;
 
 namespace Dou.Linker.Net.Test
 {
     public class ArticleTest
     {
-        [Fact]
-        
-        public void GetArticleTest() => Assert.Equal("Ola","Ola");
+        [Fact]     
+        public void GetArticleTest()
+        {
+            var file = @"C:\Projects\DOU-Linker\src\XmlSamples\Sample14.xml";
+            var processor = new XmlReader();
+            
+
+            processor.ReadXmlFile(file);
+
+            Assert.Equal(@"PORTARIA No- 13, DE 10 DE NOVEMBRO DE 2017", XmlArticleLayout.ArticleTitle);
+
+        }
+
+
+
     }
 }
